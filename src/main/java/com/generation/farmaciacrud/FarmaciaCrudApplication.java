e14bb2c67d3ed8d3f68dd091e5f621f8ebb38b93
luglifilho <luglifilho@hotmail.com>
2024-01-31 10:03:37 -0300

    Configurando o Projeto

---
+++ src/main/java/com/generation/farmaciacrud/FarmaciaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciacrud/FarmaciaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
