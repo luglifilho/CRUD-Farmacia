2810a507091d1638c012922c60edfb19e1595dbf
luglifilho <luglifilho@hotmail.com>
2024-01-31 11:59:30 -0300

    Crud Produto

---
+++ src/main/java/com/generation/farmaciacrud/model/Produto.java
@@ -0,0 +1,78 @@
+package com.generation.farmaciacrud.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotNull
+	private Float preco;
+	
+	private Boolean VendaComreceita;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public Boolean getVendaComreceita() {
+		return VendaComreceita;
+	}
+
+	public void setVendaComreceita(Boolean vendaComreceita) {
+		VendaComreceita = vendaComreceita;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+
+}
